8e6256eeb3b21c22f5363723855a68859208849e
sirig <mac@macs-MacBook-Air.local>
2023-05-08 17:58:16 +0100

    f

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,11 +15,11 @@
         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
           <!-- Styles -->
-          <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+          <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
           @livewireStyles
       
           <!-- Scripts -->
-          <script src="{{ asset('js/app.js') }}" defer></script>
+          <script src="{{ asset('/js/app.js') }}" defer></script>
       
 
         <!-- Styles -->
